6225b077a83197e32e4d5148d5eee4b37273878f
priyamal madushanka <madushanka.priyamal@yahoo.com>
2019-05-01 00:04:40 +0530

    completed setting the structure

---
+++ com.project.pos.backend/src/main/java/com/project/pos/SpringApplication.java
@@ -0,0 +1,13 @@
+package com.project.pos;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApplication {
+
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(SpringApplication.class);
+    }
+
+
+}
